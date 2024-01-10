1ee5a0dd1bf0ccaf29b9a614ac7daaa05de79886
wjmals <155592681+wjmals@users.noreply.github.com>
2024-01-11 02:58:05 +0900

    INFO: more

--- lib/main.dart
+++ lib/main.dart
@@ -455,3 +455,12 @@ class SecondScreen extends StatelessWidget {
     );
   }
 }
+
+class Third extends StatelessWidget {
+  const Third({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
